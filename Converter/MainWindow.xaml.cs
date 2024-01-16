6cd57d697b5e3622b3470d1ee8197f474cb36f93
GirayTurker <tug88418@temple.edu>
2024-01-16 12:43:01 -0500

    Add project files.

---
+++ Converter/MainWindow.xaml.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace Converter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+      
+        private const string CPassword = "admin";
+        private static bool isPasswordEntered = false;
+        //Objects For
+        //Sql Connection
+        SqlConnection con = new SqlConnection();
+        //Sql Command
+        SqlCommand cmd = new SqlCommand();  
+        //Sql adapter for communicate with data
+        SqlDataAdapter adapter = new SqlDataAdapter();
+
+        private int CurrencyId = 0;
+        private double FromAmount = 0;
+        private double TotalAmount = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetData();
+            BindCurrency();
+            Collapse.Visibility = Visibility.Collapsed;
+
+            // Check if the password was previously entered
+            //right click on project, Propeerties>Settings and enter the value for IsPasswordEntered
+            if (Properties.Settings.Default.IsPasswordEntered)
+            {
+                // Password was entered before, show the tbMaster tab
+                isPasswordEntered = true;
+                tbMaster.Visibility = Visibility.Visible;
+            }
+        }
+
+
+        //DB connection method
+        public void dbCon()
+        {
+            //ConfigurationManager is a reference type, to add it, add reference to the project
+            String Connection = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            con = new SqlConnection(Connection);
+            con.Open();
+        }
+
+        // DB fill -- CRUD Operations through UI
+        private void BindCurrency()
+        {
+            //Connect DB
+            dbCon();
+            //Create object Data Table (it is C# object)
+            //This object refresh and fill the table with using adapter fill method 
+            //with defined connection
+            DataTable dt = new DataTable();
+            //query to get data from Currency_Master table
+            cmd = new SqlCommand("select Id, CurrencyName from Currency_Master", con);
+            //CommandType define which type of command using for write a query
+            cmd.CommandType = CommandType.Text;
+
+            //adapter is accepting parameter that contains the command text of the object's selectedCommand property
+            //This DataAdapter constructor, shown in the following code, will accept a parameter that
+            //contains the command text of the object’s select command property.
+            adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(dt);
+
+            //DataRow class provides the functionality to add a new row 
+            // It allows you to add the values for each data column according to
+            // the specified data type for a data column of the data table.
+            DataRow newRow = dt.NewRow();
+            newRow["Id"] = 0;
+            newRow["CurrencyName"] = "--SELECT--";
+
+            //Insert a new row in dt with the data at a 0 position
+            dt.Rows.InsertAt(newRow, 0);
+
+            //dt is not null and rows count greater than 0
+            if (dt.Rows.Count > 0 && dt !=null)
+            {
+                //Assign the datatable data from currency combobox using ItemSource property
+                cmbFromCurrency.ItemsSource = dt.DefaultView;
+
+                cmbToCurrency.ItemsSource = dt.DefaultView;
+            }
+
+            //To display the underlying datasource for cmbFromCurrency
+            cmbFromCurrency.DisplayMemberPath = "CurrencyName";
+
+            //To use as the actual value for the items
+            cmbFromCurrency.SelectedValuePath = "Id";
+
+            //Show default item in combobox
+            cmbFromCurrency.SelectedValue = 0;
+
+            cmbToCurrency.DisplayMemberPath = "CurrencyName";
+            cmbToCurrency.SelectedValuePath = "Id";
+            cmbToCurrency.SelectedValue = 0;
+
+            con.Close();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+
+        private void cmbToCurrency_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Convert_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (txtAmount.Text == null || txtAmount.Text.Trim() == "")
+                {
+                    MessageBox.Show("Amount can not be empty", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+                    txtAmount.Focus();
+                    return;
+                }
+                else if (txtCurrencyName.Text == null || txtCurrencyName.Text.Trim() == "")
+                {
+                    MessageBox.Show("Currency Name can not be empty", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+                    txtCurrencyName.Focus();
+                    return;
+                }
+                else
+                {   
+                    //Update Button
+                    if (CurrencyId > 0)//Currency is not --SELECT--
+                    {
+                        if (MessageBox.Show("Update?", "Info", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                        {
+                            //DB changes after YES
+                            dbCon();
+                            DataTable dt = new DataTable();
+                            cmd = new SqlCommand("UPDATE Currency_Master SET Amount = @Amount, CurrencyName = @CurrencyName WHERE Id = @Id", con);
+                            cmd.CommandType = CommandType.Text;
+                            cmd.Parameters.AddWithValue("@Id", CurrencyId);
+                            cmd.Parameters.AddWithValue("@Amount", txtAmount.Text);
+                            cmd.Parameters.AddWithValue("@CurrencyName", txtCurrencyName.Text);
+                            cmd.ExecuteNonQuery();
+                            con.Close();
+
+                            MessageBox.Show("Data update is Successfull", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                    }
+
+                    //Save Button
+                    else 
+                    {
+                        if (MessageBox.Show("Save?", "Info", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                        {
+                            //DB changes after YES
+                            dbCon();
+                            DataTable dt = new DataTable();
+                            cmd = new SqlCommand("INSERT INTO Currency_Master(Amount, CurrencyName) VALUES(@Amount, @CurrencyName)", con);
+                            cmd.CommandType = CommandType.Text;
+                            cmd.Parameters.AddWithValue("@Amount", txtAmount.Text);
+                            cmd.Parameters.AddWithValue("@CurrencyName", txtCurrencyName.Text);
+                            cmd.ExecuteNonQuery();
+                            con.Close();
+
+                            MessageBox.Show("Data save is Successfull", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                    }
+                    ClearCurrencyMaster();
+                }
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        //Method to clear all user entries
+        private void ClearCurrencyMaster()
+        {
+            try
+            {
+                txtAmount.Text = string.Empty;
+                txtCurrencyName.Text = string.Empty;
+                btnSave.Content = "Save";
+                GetData();
+                CurrencyId=0;
+                BindCurrency();
+                txtAmount.Focus();
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        //Bind Data in DataGrid View
+        private void GetData()
+        {
+            dbCon();
+            DataTable dt = new DataTable();
+            cmd = new SqlCommand("SELECT * FROM Currency_Master", con);
+            cmd.CommandType = CommandType.Text;
+            adapter=new SqlDataAdapter(cmd);
+            adapter.Fill(dt);
+            if (dt != null && dt.Rows.Count > 0)
+            {
+                dgvCurrency.ItemsSource = dt.DefaultView;
+            }
+            else 
+            {
+                dgvCurrency.ItemsSource = null;
+            }
+
+            con.Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dgvCurrency_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+
+        }
+
+        private void cmbToCurrency_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cmbFromCurrency_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void cmbFromCurrency_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+       
+
+        private void Tab_Control_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Check if the selected tab is the protected tab
+            if (tbMaster.IsSelected)
+            {
+                if (isPasswordEntered == true)
+                {
+                    tbMaster.Visibility = Visibility.Visible;
+                }
+                else
+                {  // Prompt for password
+                    ShowPasswordPrompt();
+                }
+            }
+            
+        }
+        private void ShowPasswordPrompt()
+        {
+            bool passwordEntered = false;
+            do 
+            {
+                // Create an instance of the PasswordDialog window
+                PasswordDialog passwordDialog = new PasswordDialog();
+
+                // Show the dialog and get the result
+                bool? dialogResult = passwordDialog.ShowDialog();
+
+                // Check if the dialog was accepted and the password is correct
+                if (dialogResult == true && passwordDialog.Password == CPassword)
+                {
+                    isPasswordEntered = true;
+                    Collapse.Visibility = Visibility.Visible;
+                    passwordEntered = true;
+                }
+                else
+                {
+                    // Password is incorrect, ask if the user wants to try again
+                    MessageBoxResult result = MessageBox.Show("Incorrect password. Try again?", "Access Denied", MessageBoxButton.YesNo);
+
+                    // If user chooses No, exit the loop
+                    if (result == MessageBoxResult.No)
+                    {
+                        tbMain.SelectedItem = tbConverter; // Go back to the default tab
+                        break; 
+                    }       
+                }
+            }
+            
+            while (!passwordEntered) ;
+        }
+        // This method can be called from outside to check if the password is entered
+        //public static bool CheckPasswordEntered()
+        //{
+        //    return isPasswordEntered;    
+        //}
+    }
+}
---
+++ Converter/PasswordDialog.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Converter
+{
+    /// <summary>
+    /// Interaction logic for PasswordDialog.xaml
+    /// </summary>
+    public partial class PasswordDialog : Window
+    {
+        public string Password { get; private set; }
+        public PasswordDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            // Set the Password property and close the window
+            Password = passwordBox.Password;
+            DialogResult = true;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            // Close the window without setting the password
+            DialogResult = false;
+        }
+    }
+}
